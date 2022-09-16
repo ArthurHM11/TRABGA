edc290b717fe61bccfa2249ea6005477d27b1d88
ArthurHM11 <113645946+ArthurHM11@users.noreply.github.com>
2022-09-15 22:28:33 -0300

    Update trabaga.py

--- trabaga.py
+++ trabaga.py
@@ -35,6 +35,7 @@
 
 import os
 
+
 valorvenda = 0
 
 # cod1 = ('Calça')
@@ -57,25 +58,6 @@ cod7p = 12.99
 cod8p = 183.00
 cod9p = 219.00
 
-# global cod1
-# global cod2
-# global cod3
-# global cod4
-# global cod5
-# global cod6
-# global cod7
-# global cod8
-# global cod9
-
-# global cod1p
-# global cod2p
-# global cod3p
-# global cod4p
-# global cod5p
-# global cod6p
-# global cod7p
-# global cod8p
-# global cod9p
 
 est1 = 20
 est2 = 18
@@ -87,15 +69,6 @@ est7 = 17
 est8 = 8
 est9 = 3
 
-# global est1
-# global est2
-# global est3
-# global est4
-# global est5
-# global est6
-# global est7
-# global est8
-# global est9
 
 print('inicializando sistema...')
 nome = input('Boa noite.\nPor favor, insira seu nome: ')
@@ -113,11 +86,29 @@ def menu():
   return item
 
 # Uma função para cada item do menu
-def opcao_1(est1,est2,est3,est4,est5,est6,est7,est8,est9):
+def opcao_1():
+  global cod1p
+  global cod2p
+  global cod3p
+  global cod4p
+  global cod5p
+  global cod6p
+  global cod7p
+  global cod8p
+  global cod9p
+  global est1
+  global est2
+  global est3
+  global est4
+  global est5
+  global est6
+  global est7
+  global est8
+  global est9
   print('\nOpção escolhida: 1\n')
-  nomec = input('Nome do Cliente: ')
+  nomec = input('insira o nome do cliente: ')
   print('O que será comprado?')
-  print('Código Produto Estoque Valor\n1 Calça 20 R$ 112,00\n2 Camisa 18 R$ 95,00\n3 Bermuda 23 R$ 49,90\n4 Saia 12 R$ 169,00\n5 Blusa 9 R$ 120,00\n6 Moletom 4 R$ 135,00\n7 Meia 17 R$ 12,99\n8 Tênis 8 R$ 183,00\n9 Bota 3 R$ 219,90\n')
+  print('Código Produto Estoque Valor\n1 - Calça {} R$ 112,00\n2 - Camisa {} R$ 95,00\n3 - Bermuda {} R$ 49,90\n4 - Saia {} R$ 169,00\n5 - Blusa {} R$ 120,00\n6 - Moletom {} R$ 135,00\n7 - Meia {} R$ 12,99\n8 - Tênis {} R$ 183,00\n9 - Bota {} R$ 219,90\n10 - Terminar compra.'.format(est1,est2,est3,est4,est5,est6,est7,est8,est9))
   codv1 = 0
   codv2 = 0
   codv3 = 0
@@ -128,63 +119,108 @@ def opcao_1(est1,est2,est3,est4,est5,est6,est7,est8,est9):
   codv8 = 0
   codv9 = 0
   vvenda = 0
-
-  codp = input('Insira o código do produto: ')
+  codp = '0'
   while(codp != '10'):
+    codp = input('Insira o código do produto: ')
     if codp == '1':
-      vvenda += cod1p
-      codv1 += 1
-      est1 -= 1
-      print('Uma Calça de R$112,00 foi comprada.')
+      if est1 > 0:
+        vvenda += cod1p
+        codv1 += 1
+        est1 -= 1
+        print('Uma Calça de R$112,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '2':
-      vvenda += cod2p
-      codv2 += 1
-      est2 -= 1
-      print('Uma Camisa de R$95,00 foi comprada.')
+      if est2 > 0:
+        vvenda += cod2p
+        codv2 += 1
+        est2 -= 1
+        print('Uma Camisa de R$95,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '3':
-      vvenda += cod3p
-      codv3 += 1
-      est3 -= 1
-      print('Uma Bermuda de R$49,90 foi comprada.')
+      if est3 > 0:
+        vvenda += cod3p
+        codv3 += 1
+        est3 -= 1
+        print('Uma Bermuda de R$49,90 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '4':
-      vvenda += cod4p
-      codv4 += 1
-      est4 -= 1
-      print('Uma Saia de R$169,00 foi comprada.')
+      if est4 > 0:
+        vvenda += cod4p
+        codv4 += 1
+        est4 -= 1
+        print('Uma Saia de R$169,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '5':
-      vvenda += cod5p
-      codv5 += 1
-      est5 -= 1
-      print('Uma Blusa de R$120,00 foi comprada.')
+      if est5 > 0:
+        vvenda += cod5p
+        codv5 += 1
+        est5 -= 1
+        print('Uma Blusa de R$120,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '6':
-      vvenda += cod6p
-      codv6 += 1
-      est6 -= 1
-      print('Um Moletom de R$135,00 foi comprada.')
+      if est6 > 0:
+        vvenda += cod6p
+        codv6 += 1
+        est6 -= 1
+        print('Um Moletom de R$135,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '7':
-      vvenda += cod7p
-      codv7 += 1
-      est7 -= 1
-      print('Uma Meia de R$12,99 foi comprada.')
+      if est7 > 0:
+        vvenda += cod7p
+        codv7 += 1
+        est7 -= 1
+        print('Uma Meia de R$12,99 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '8':
-      vvenda += cod8p
-      codv8 += 1
-      est8 -= 1
-      print('Um Tenis de R$183,00 foi comprado.')
+      if est8 > 0:
+        vvenda += cod8p
+        codv8 += 1
+        est8 -= 1
+        print('Um Tenis de R$183,00 foi comprado.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '9':
-      vvenda += cod9p
-      codv9 += 1
-      est9 -= 1
-      print('Uma Bota de R$219,00 foi comprada.')
+      if est9 > 0:
+        vvenda += cod9p
+        codv9 += 1
+        est9 -= 1
+        print('Uma Bota de R$219,00 foi comprada.')
+      else:
+        print('não há produtos em estoque o suficiente.')
     elif codp == '10':
-      print('\nSaindo...\n')
+      print('\nFinalizando Compra...\n')
     else:
       print('\nOpção desconhecida!\n')
   compratotal = print('Total da venda:\nCliente: {}\nValor Total R${:.2f}'.format(nomec,vvenda))
+  print('Calças em Estoque: {}'.format(est1))
+  print('Camisa em Estoque: {}'.format(est2))
+  print('Bermudas em Estoque: {}'.format(est3))
+  print('Saias em Estoque: {}'.format(est4))
+  print('Blusas em Estoque: {}'.format(est5))
+  print('Moletons em Estoque: {}'.format(est6))
+  print('Meias em Estoque: {}'.format(est7))
+  print('Tenis em Estoque: {}'.format(est8))
+  print('Botas em Estoque: {}'.format(est9))
   return compratotal
 
 
 def opcao_2():
+  global est1
+  global est2
+  global est3
+  global est4
+  global est5
+  global est6
+  global est7
+  global est8
+  global est9
   print('\nOpção escolhida: 2\n')
 
 
@@ -206,7 +242,7 @@ escolha = '0'
 while(escolha != '9'):
   escolha = menu()
   if escolha == '1':
-    opcao_1(est1,est2,est3,est4,est5,est6,est7,est8,est9)
+    opcao_1()
   elif escolha == '2':
     opcao_2()
   elif escolha == '3':
