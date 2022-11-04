002f9f855b14c69bbf587c7fcafbccfaf88fe1bc
PauloSilveira50 <paulohenriquecs555@gmail.com>
2022-11-03 21:46:03 -0300

    sdada

--- trabaga.py
+++ trabaga.py
@@ -83,9 +83,11 @@ else: print('Algo está errado!')
 # Bagulho 2
 c1 = input('Informe uma cidade de origem: ')
 c2 = input('Informe uma cidade de destino: ')
-if c1 is not 'in' 'arquivo' or c2 is not 'in' 'arquivo':
-    print('Cidade não encontrada')
-    dsada
+while c1 != 'in' 'arquivo' or c2 != 'in' 'arquivo' or c1 == c2:
+  print('Cidade não encontrada ou origem e destinos iguais.')
+  print('Por favor, insira uma nova cidade')
+  
+  print('#calcular e mostrar na tela a distância rodoviária entre elas e o custo total do trecho#.')
 
 
 
