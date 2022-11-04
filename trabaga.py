e8a25bbd9e1373b65e4d55b95a00e5c034f3df5e
Maicon3DR <108578305+Maicon3DR@users.noreply.github.com>
2022-11-03 21:16:51 -0300

    Ok

--- trabaga.py
+++ trabaga.py
@@ -57,7 +57,4 @@ if CustoPorKMrodado >= 0:
   print('Valor aceito!')
 elif CustoPorKMrodado < 0:
   print('O valor deverá ser maior ou igual a zero!')
-elif CustoPorKMrodado >= 'A' and 'Z':
-  print('Não pode ser letras, apenas números maiores que zero')
-
 else: print('Algo está errado!')
